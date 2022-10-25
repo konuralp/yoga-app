8283ded9e993dd76c0c046f53b8e2006c1d5f027
Konuralp <36452458+konuralp@users.noreply.github.com>
2022-10-24 22:30:33 -0500

    Popup Settings

--- app/src/main/java/com/example/yoga/MainActivity.kt
+++ app/src/main/java/com/example/yoga/MainActivity.kt
@@ -7,5 +7,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setTheme(getPreferences(MODE_PRIVATE).getInt("theme", R.style.Theme_Yoga))
+
+//        val fragment = PopupSettingsFragment()
+//        val transaction = supportFragmentManager.beginTransaction()
+//        transaction.add(R.id.frameLayout, fragment)
+//        transaction.commit()
+
+        PopupSettingsFragment().show(supportFragmentManager, PopupSettingsFragment.TAG)
     }
 }
